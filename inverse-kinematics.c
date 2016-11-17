f266a06cde9834e95a021fe00aad6ea22182d599
Thomas <thomas-hiemstra@hotmail.com>
2016-11-17 23:22:23 +0100

    fixed IK function, added line function

--- inverse-kinematics.c
+++ inverse-kinematics.c
@@ -16,8 +16,8 @@
 #define d1  10.2   //ground to q1
 #define d6  12.6 //gripper to wrist
 #define a_2 14    //q1 to q2
-#define a_3 13.6  //q2 to wrist
-#define pi  3.14159265358979323846264338327950288419716939937510L
+#define a_3 14.2  //q2 to wrist
+#define pi  3.14159265358979323846264338327950288419716939937510
 #define degtorad 0.0174532925
 #define radtodeg 57.29577951
 int ticks[7];
@@ -29,6 +29,19 @@ long double servovals[7][5] =  {{135,245,355,465,577}, //0
                                 {100,215,320,435,555}, //4
                                 {115,235,345,460,565}, //5
                                 {160,270,370,470,580}};//6
+
+
+struct Pos{
+    long double x; long double y; long double z;
+
+};
+
+
+
+void circle(long double start[3][4],long double centre[3],long double part,int direction){
+
+}
+
 
 void normalize(float *vec){
   int a1;
@@ -93,59 +106,60 @@ void inverseKinematics(long double x,long double y,long double z,long double t[3
 {  //t being the target rotation matrix
   //angles[i] are in radians!
 
-  int i1;
-  long double xc,yc,zc;
-  long double R23,R13,R33,R32,R31;
-  long double r11,r12,r13,r21,r22,r23,r31,r32,r33;
-  long double q1,q2,q3;
-  long double D,k1,k2;
-  xc = x - d6*t[0][2];
-  yc = y - d6*t[1][2];
-  zc = z - d6*t[2][2];
-  D = ( pow(xc,2) + pow(yc,2) + pow((zc-d1),2) - pow(a_2,2) - pow(a_3,2) )/(2*a_2*a_3);
-  angles[1] = atan2l(yc,xc);
-  angles[3] = atan2l( -sqrt( 1 - pow(D,2) ),D );
-  k1 = a_2+a_3*cosl(angles[3]);
-  k2 = a_3*sinl(angles[3]);
-  angles[2] = atan2l( (zc-d1), sqrt(pow(xc,2) + pow(yc,2)) ) - atan2l(k2,k1) ;
-  angles[3] = angles[3] + pi/2;
 
-  //for my own sanity:
-  q1=angles[1]; q2=angles[2]; q3=angles[3];
-  r11=t[0][0];r12=t[0][1];r13=t[0][2]; r21=t[1][0];r22=t[1][1];r23=t[1][2]; r31=t[2][0];r32=t[2][1];r33=t[2][2];
+    long double xc,yc,zc;
+    long double R23,R13,R33,R32,R31;
+    long double r11,r12,r13,r21,r22,r23,r31,r32,r33;
+    long double q1,q2,q3;
+    long double D,k1,k2;
+    xc = x - d6*t[0][2];
+    yc = y - d6*t[1][2];
+    zc = z - d6*t[2][2];
+    D = ( pow(xc,2) + pow(yc,2) + pow((zc-d1),2) - pow(a_2,2) - pow(a_3,2) )/(2*a_2*a_3);
+    angles[1] = atan2l(yc,xc);
+    angles[3] = atan2l( -sqrt( 1 - pow(D,2) ),D );
+    k1 = a_2+a_3*cosl(angles[3]);
+    k2 = a_3*sinl(angles[3]);
+    angles[2] = atan2l( (zc-d1), sqrt(pow(xc,2) + pow(yc,2)) ) - atan2l(k2,k1) ;
+    angles[3] = angles[3] + pi/2;
+    //for my own sanity:
+    q1=angles[1]; q2=angles[2]; q3=angles[3];
+    r11=t[0][0];r12=t[0][1];r13=t[0][2]; r21=t[1][0];r22=t[1][1];r23=t[1][2]; r31=t[2][0];r32=t[2][1];r33=t[2][2];
 
-  /* dummys to keep things organized. Rij from transpose(R30).R: */
-  R13 = r13*cosl(q1)*cosl(q2 + q3) + r23*cosl(q2 + q3)*sinl(q1) + r33*sinl(q2 + q3);
-  R23 = -r23*cosl(q1) + r13*sinl(q1);
-  R33 = -r33*cosl(q2 + q3) + r13*cosl(q1)*sinl(q2 + q3) + r23*sinl(q1)*sinl(q2 + q3);
-  R32 = -r32*cosl(q2 + q3) + r12*cosl(q1)*sinl(q2 + q3) + r22*sinl(q1)*sinl(q2 + q3);
-  R31 = -r31*cosl(q2 + q3) + r11*cosl(q1)*sinl(q2 + q3) + r21*sinl(q1)*sinl(q2 + q3);
+    /* dummys to keep things organized. Rij from transpose(R30).R: */
+    R13 = r13*cosl(q1)*cosl(q2 + q3) + r23*cosl(q2 + q3)*sinl(q1) + r33*sinl(q2 + q3);
+    R23 = -r23*cosl(q1) + r13*sinl(q1);
+    R33 = -r33*cosl(q2 + q3) + r13*cosl(q1)*sinl(q2 + q3) + r23*sinl(q1)*sinl(q2 + q3);
+    R32 = -r32*cosl(q2 + q3) + r12*cosl(q1)*sinl(q2 + q3) + r22*sinl(q1)*sinl(q2 + q3);
+    R31 = -r31*cosl(q2 + q3) + r11*cosl(q1)*sinl(q2 + q3) + r21*sinl(q1)*sinl(q2 + q3);
 
-  /* these angles are just the ZYZ euler angles */
-  angles[4] = atan2l(-R23,-R13);
-  angles[5] = atan2l(-sqrt(R13*R13+R23*R23),R33);
-  angles[6] = atan2l(-R32,R31);
+    /* these angles are just the ZYZ euler angles */
+    angles[4] = atan2l(-R23,-R13);
+    angles[5] = atan2l(-sqrt(R13*R13+R23*R23),R33);
+    angles[6] = atan2l(-R32,R31);
 
-  char name[1024];
-  sprintf(name,"angles");
-  FILE *file;
-  file = fopen(name,"wb");
+    if (angles[4] > pi/2){
+    angles[4] = angles[4] - pi;
+    angles[5] = -angles[5];
+    angles[6] = pi + angles[6];
+    }
+    if (angles[4] < -pi/2){
+    angles[4] = angles[4] + pi;
+    angles[5] = -angles[5];
+    angles[6] = angles[6] -pi;
+    }
 
-
-  if (angles[4] > pi/2){
-    angles[4] =   angles[4] - pi;
-    angles[5] = -(pi/2)- angles[5];
-  }
-  if (angles[4] < -pi/2){
-    angles[4] =   angles[4] + pi;
-    angles[5] = - (pi/2) - angles[5];
-  }
-  /* adjust angles for the servos (some servos are in different orientations than DH frames) */
-  angles[1] = angles[1];
-  angles[2] = angles[2];
-  angles[3] = -(angles[3] - pi/2);
-  angles[4] = (angles[4] + pi/2);
-  angles[5] = -angles[5];
-  angles[6] = (pi/2)-angles[6];
+    /* adjust angles for the servos (some servos are in different orientations than DH frames) */
+    angles[1] = angles[1];
+    angles[2] = angles[2];
+    angles[3] = -(angles[3] - pi/2);
+    angles[4] = (angles[4] + pi/2);
+    angles[5] = (pi/2) - angles[5];
+    angles[6] = (pi/2) + angles[6];
 
 }
+
+
+
+
+
--- main.c
+++ main.c
@@ -9,16 +9,20 @@
 #define pi  3.14159265358979323846264338327950288419716939937510L
 #define degtorad 0.0174532925
 #define radtodeg 57.29577951
-float i;
+#define precission 4   /* amount of steps per centimeter */
+#define mindelay 30.0    /* determines the max velocity of the end effector */
+#define increase 4.0      /* I choose to lowest speed to be a factor of 4 smaller than the max speed */
+#define ramp 1.0          /* distance over which to increase/decrease speed */
+int i;
 double angles[7];
 int ticks[7];
 int fd;
 long double temp;
-long double r[3][3]= {{0,1,0},    //the target rotation matrix R
+long double t[3][3]= {{0,1,0},    //the target rotation matrix R
                       {0,0,1},
-                      {-1,0,0}};
+                      {1,0,0}};
 long double w[3][3]= {{0,1,0},    //the target rotation matrix R
-                      {-1,0,0},
+                      {1,0,0},
                       {0,0,-1}};
 /*TODO: XYZ euler matrix with gripper always parallel to ground (roll=0)*/
 /*give (non normalized ) aproach and sliding vector, make rotation matrix */
@@ -26,48 +30,136 @@ long double w[3][3]= {{0,1,0},    //the target rotation matrix R
 /*implement acceleration and decelleration*/
 /*jacobian?*/
 
+struct Pos{
+    long double x; long double y; long double z;
+    long double alpha; long double beta; long double gamma;
+};
+
 void nsleep(long ms)
 {
     long us;
     us = 1000*ms;
     struct timespec wait;
-
     wait.tv_sec = us / (1000 * 1000);
     wait.tv_nsec = (us % (1000 * 1000)) * 1000;
     nanosleep(&wait, NULL);
 }
 
 
+void line(struct Pos start, struct Pos stop){
+    double j;
+    double dx,dy,dz,r,dr,x,y,z;
+    double delay,current_r;
+    int steps;
+    dx = stop.x - start.x;
+    dy = stop.y - start.y;
+    dz = stop.z - start.z;
+
+    r = sqrt(dx*dx+dy*dy+dz*dz); /* total path length */
+    steps = r*precission;
+    dr = r/steps;
+
+    /* v=a*r²+b for r<=ramp, v=-c*r²+d for r>= ramp*/
+    double a = (1.0/(ramp*ramp) )*( (dr/mindelay)-(dr/(increase*mindelay)) );
+    double b = dr/(increase*mindelay);
+    double c = ( (dr/mindelay) - (dr/(increase*mindelay)) ) / (2*r*ramp - ramp*ramp);
+    double d = (dr/(increase*mindelay)) + c*r*r;
+
+    for(j = 0; j <= steps; j++){
+        x = start.x + ((j/steps)*dx);
+        y = start.y + ((j/steps)*dy);
+        z = start.z + ((j/steps)*dz);
+        current_r = dr*j;
+        inverseKinematics(x,y,z,t,angles);
+        commandArduino(fd,angles);
+
+    /* mess due to speed regulation */
+    /* v = dr/delay so delay = dr/v */
+    if(r<=2*ramp){ /* path too short, v = constant */
+        nsleep(mindelay);
+    }
+    else if(r>2*ramp){  /* v = a*current_r² + b*/
+        if (current_r <= ramp){
+            delay = dr / ( a*current_r*current_r + b);
+            nsleep(delay);
+        }
+        else if(current_r>ramp && current_r<r-ramp){
+            nsleep(delay);
+        }
+        else if(current_r >= r-ramp){ /*v = -c*current_r² + d */
+            delay = dr/( -c*current_r*current_r + d);
+            nsleep(delay);
+        }
+    }
+    /* end of mess */
+    }
+
+}
+
+
 int main()
 {
-    int delay=80;
+    struct Pos start;
+    struct Pos stop;
+    start.x = -10; start.y = 30; start.z = 25;
+    stop.x  = 10; stop.y  = 30; stop.z  = 25;
 
-    fd = serialport_init("/dev/ttyUSB0", 9600);
+    struct Pos bottom1;
+    struct Pos bottom2;
+    bottom1.x = -10; bottom1.y = 30; bottom1.z = 5;
+    bottom2.x = 10; bottom2.y = 30; bottom2.z = 5;
+
+    fd = serialport_init("/dev/ttyUSB0", 115200);
     sleep(2);
-    inverseKinematics(10,30,10,r,angles); /* dumps the results in angles */
+    inverseKinematics(-10,30,5,t,angles);
     commandArduino(fd, angles);
-    nsleep(1000);
-    for(i=20;i<=40;i++){
-        inverseKinematics(10,30,i/2,r,angles); /* dumps the results in angles */
-        commandArduino(fd, angles);
-        nsleep(delay);
-    }
-    for(i=20;i>=-20;i--){
-        inverseKinematics(i/2,30,20,r,angles); /* dumps the results in angles */
-        commandArduino(fd, angles);
-        nsleep(delay);
-    }
-    for(i=40;i>=20;i--){
-        inverseKinematics(-10,30,i/2,r,angles); /* dumps the results in angles */
-        commandArduino(fd, angles);
-        nsleep(delay);
-    }
-    for(i=-20;i<=20;i++){
-        inverseKinematics(i/2,30,10,r,angles); /* dumps the results in angles */
-        commandArduino(fd, angles);
-        nsleep(delay);
-    }
+    sleep(1);
+
+    line(bottom1,start);
+    line(start,stop);
+    line(stop,start);
+    line(start,stop);
+    line(stop,start);
+    line(start,bottom1);
+
+//    line(bottom1,start,fd);
+//    line(start,stop,fd);
+//    line(stop,bottom2,fd);
+//    line(bottom2,stop,fd);
+//    line(stop,start,fd);
+//    line(start,bottom1,fd);
 
     sleep(2);
     serialport_close(fd);
+
+//    int delay = 80;
+//    fd = serialport_init("/dev/ttyUSB0", 9600);
+//
+//    sleep(2);
+//    inverseKinematics(10,30,10,r,angles); /* dumps the results in angles */
+//    commandArduino(fd, angles);
+//    nsleep(1000);
+//    for(i=20;i<=40;i++){
+//        inverseKinematics(10,30,i/2,r,angles); /* dumps the results in angles */
+//        commandArduino(fd, angles);
+//        nsleep(delay);
+//    }
+//    for(i=20;i>=-20;i--){
+//        inverseKinematics(i/2,30,20,r,angles); /* dumps the results in angles */
+//        commandArduino(fd, angles);
+//        nsleep(delay);
+//    }
+//    for(i=40;i>=20;i--){
+//        inverseKinematics(-10,30,i/2,r,angles); /* dumps the results in angles */
+//        commandArduino(fd, angles);
+//        nsleep(delay);
+//    }
+//    for(i=-20;i<=20;i++){
+//        inverseKinematics(i/2,30,10,r,angles); /* dumps the results in angles */
+//        commandArduino(fd, angles);
+//        nsleep(delay);
+//    }
+//
+//    sleep(2);
+//    serialport_close(fd);
 }

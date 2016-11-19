fc60d14c7d42e5c84b83376ce35bf84f095b3836
Thomas <thomas-hiemstra@hotmail.com>
2016-11-19 23:48:19 +0100

    added more descriptions

--- inverse-kinematics.c
+++ inverse-kinematics.c
@@ -20,8 +20,9 @@
 #define pi  3.14159265358979323846264338327950288419716939937510
 #define degtorad 0.0174532925
 #define radtodeg 57.29577951
-int ticks[7];
-long double inangles[5]      =  {0,pi/4.0,pi/2.0,(3.0/4.0)*pi,pi};
+int ticks[7]; /* pwm signals for the servos */
+/* arrays for the multimap function which makes the servos a bit more linear */
+long double inangles[5]      =  {0,pi/4.0, pi/2.0, (3.0/4.0)*pi, pi};
 long double servovals[7][5] =   {{135,245,355,465,577}, //0
                                  {150,265,370,475,590}, //1
                                  {167,285,385,490,650}, //2
@@ -30,34 +31,14 @@ long double servovals[7][5] =   {{135,245,355,465,577}, //0
                                  {115,235,345,460,565}, //5
                                  {160,270,370,470,580}};//6
 
-
 struct Pos{
     long double x; long double y; long double z;
-
+    long double alpha; long double beta; long double gamma;
 };
 
 
-
 void circle(long double start[3][4],long double centre[3],long double part,int direction){
-
-}
-
-
-void normalize(float *vec){
-  int a1;
-  float moo = 0;
-  for(a1=0; a1<3; a1++){
-    moo += vec[a1]*vec[a1];
-  }
-  for(a1=0; a1<3; a1++){
-    vec[a1] = vec[a1]/ sqrt(moo);
-  }
-}
-
-void cross(float *vec1, float *vec2, float *res){
-  res[0] = vec1[1]*vec2[2] - vec1[2]*vec2[1];
-  res[1] = vec1[2]*vec2[0] - vec1[0]*vec2[2];
-  res[2] = vec1[0]*vec2[1] - vec1[1]*vec2[0];
+    /* work in progress */
 }
 
 void sendStuff(int fd, short val){
@@ -101,20 +82,18 @@ int getServoTick(float val, int servoNumber){ /* a multimap from angles to servo
   return roundl((val - inangles[pos-1]) * (servovals[servoNumber][pos] - servovals[servoNumber][pos-1]) / (inangles[pos] - inangles[pos-1]) + servovals[servoNumber][pos-1]);
 }
 
-
-void inverseKinematics(long double x,long double y,long double z,long double t[3][3],double angles[7])
-{  //t being the target rotation matrix
-  //angles[i] are in radians!
-
-
+void inverseKinematics(long double x,long double y,long double z,long double t[3][3],double angles[7]) /*results are stored in angles array */
+{
     long double xc,yc,zc;
     long double R23,R13,R33,R32,R31;
     long double r11,r12,r13,r21,r22,r23,r31,r32,r33;
     long double q1,q2,q3;
     long double D,k1,k2;
+    /* compute the wrist position */
     xc = x - d6*t[0][2];
     yc = y - d6*t[1][2];
     zc = z - d6*t[2][2];
+    /* solve inverse kinematics for the first 3 angles */
     D = ( pow(xc,2) + pow(yc,2) + pow((zc-d1),2) - pow(a_2,2) - pow(a_3,2) )/(2*a_2*a_3);
     angles[1] = atan2l(yc,xc);
     angles[3] = atan2l( -sqrt( 1 - pow(D,2) ),D );
@@ -122,25 +101,23 @@ void inverseKinematics(long double x,long double y,long double z,long double t[3
     k2 = a_3*sinl(angles[3]);
     angles[2] = atan2l( (zc-d1), sqrt(pow(xc,2) + pow(yc,2)) ) - atan2l(k2,k1) ;
     angles[3] = angles[3] + pi/2;
-    //for my own sanity:
+
+    /* for my own sanity */
     q1=angles[1]; q2=angles[2]; q3=angles[3];
     r11=t[0][0];r12=t[0][1];r13=t[0][2]; r21=t[1][0];r22=t[1][1];r23=t[1][2]; r31=t[2][0];r32=t[2][1];r33=t[2][2];
 
-    /* dummys to keep things organized. Rij from transpose(R30).R: */
+    /* solve inverse kinematics for the final 3 angles, for details see:
+    Modelling and Control of Robot Manipulators, Lorenzo Sciavicco and Bruno Siciliano chapter 2*/
     R13 = r13*cosl(q1)*cosl(q2 + q3) + r23*cosl(q2 + q3)*sinl(q1) + r33*sinl(q2 + q3);
     R23 = -r23*cosl(q1) + r13*sinl(q1);
     R33 = -r33*cosl(q2 + q3) + r13*cosl(q1)*sinl(q2 + q3) + r23*sinl(q1)*sinl(q2 + q3);
     R32 = -r32*cosl(q2 + q3) + r12*cosl(q1)*sinl(q2 + q3) + r22*sinl(q1)*sinl(q2 + q3);
     R31 = -r31*cosl(q2 + q3) + r11*cosl(q1)*sinl(q2 + q3) + r21*sinl(q1)*sinl(q2 + q3);
 
-    /* these angles are just the ZYZ euler angles */
     angles[4] = atan2l(-R23,-R13);
     angles[5] = atan2l(-sqrt(R13*R13+R23*R23),R33);
     angles[6] = atan2l(-R32,R31);
-
-
-
-
+    /* servo's can only move 180 degrees*/
     if (angles[4] > pi/2){
     angles[4] = angles[4] - pi;
     angles[5] = -angles[5];
@@ -151,17 +128,11 @@ void inverseKinematics(long double x,long double y,long double z,long double t[3
     angles[5] = -angles[5];
     angles[6] = pi + angles[6];
     }
-    /* adjust angles for the servos (some servos are in different orientations than DH frames) */
+    /* some servo's are oriented different than the DH frames, so angles are adjusted for that here*/
     angles[1] = angles[1];
     angles[2] = angles[2];
     angles[3] = -(angles[3] - pi/2);
     angles[4] = (angles[4] + pi/2);
     angles[5] = (pi/2) - angles[5];
     angles[6] = fmod( ((pi/2) + angles[6] ), 2.0*pi) ;
-
 }
-
-
-
-
-
--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include "arduino-serial-lib.h"
 #include "inverse-kinematics.h"
+
 #define pi  3.14159265358979323846264338327950288419716939937510
 #define degtorad 0.0174532925
 #define radtodeg 57.29577951
@@ -13,11 +14,11 @@
 #define mindelay 30.0    /* determines the max velocity of the end effector */
 #define increase 4.0      /* I choose to lowest speed to be a factor of 4 smaller than the max speed */
 #define ramp 1.0          /* distance over which to increase/decrease speed */
-double  dr = 1.0/precision;
+double  dr = 1.0/precision; /* step size for the line function*/
 int i;
 double angles[7];
 int ticks[7];
-int fd;
+int fd;  /* for serial communication */
 long double temp;
 long double t[3][3]= {{0,1,0},    //the target rotation matrix R
                       {0,0,1},
@@ -25,12 +26,12 @@ long double t[3][3]= {{0,1,0},    //the target rotation matrix R
 long double w[3][3]= {{0,1,0},    //the target rotation matrix R
                       {1,0,0},
                       {0,0,-1}};
+
 /*to do: jacobian*/
 
 /* roll pitch yaw matrix with the columns permutated z->y, y->x x->z */
 /* so that by default the gripper is in the y_0 direction and the  slider is in the x_0 direction */
 void eulerMatrix(long double alpha, long double beta, long double gamma, long double m[3][3]){
-
     long double ca,cb,cg,sa,sb,sg;
     ca = cosl(alpha); cb = cosl(beta); cg = cosl(gamma);
     sa = sinl(alpha); sb = sinl(beta); sg = sinl(gamma);
@@ -47,12 +48,12 @@ void eulerMatrix(long double alpha, long double beta, long double gamma, long do
     m[2][2] = sa*sb+ca*sg;
 }
 
-struct Pos{
+struct Pos{  /* I decided to put the 6 inputs for position in 1 struct for clarity */
     long double x; long double y; long double z;
-    long double alpha; long double beta; long double gamma;
+    long double alpha; long double beta; long double gamma; /* euler angles for the target orientation */
 };
 
-void nsleep(long ms)
+void msleep(long ms)  /* delay function in miliseconds*/
 {
     long us;
     us = 1000*ms;
@@ -72,7 +73,7 @@ void line(struct Pos start, struct Pos stop){
     dz = stop.z - start.z;
     r = sqrt(dx*dx+dy*dy+dz*dz); /* total path length */
     steps = r*precision;
-    /* v=a*r²+b for r<=ramp, v=-c*r²+d for r>= ramp*/
+    /* v=a*r²+b for current_r<=ramp, v=-c*r²+d for current_r>= r - ramp*/
     double a = (1.0/(ramp*ramp) )*( (dr/mindelay)-(dr/(increase*mindelay)) );
     double b = dr/(increase*mindelay);
     double c = ( (dr/mindelay) - (dr/(increase*mindelay)) ) / (2*r*ramp - ramp*ramp);
@@ -83,39 +84,37 @@ void line(struct Pos start, struct Pos stop){
         y = start.y + ((j/steps)*dy);
         z = start.z + ((j/steps)*dz);
         current_r = dr*j;
+
         inverseKinematics(x,y,z,t,angles);
         commandArduino(fd,angles);
 
     /* mess due to accelleration */
-    /* v = dr/delay so delay = dr/v */
     if(r<=2*ramp){ /* path too short, v = constant */
-        nsleep(mindelay);
+        msleep(mindelay);
     }
     else if(r>2*ramp){  /* v = a*current_r² + b*/
         if (current_r <= ramp){
-            delay = dr / ( a*current_r*current_r + b);
-            nsleep(delay);
+            delay = dr / ( a*current_r*current_r + b);  /* v = dr/delay so delay = dr/v */
+            msleep(delay);
         }
         else if(current_r>ramp && current_r<r-ramp){
-            nsleep(delay);
+            msleep(delay);
         }
         else if(current_r >= r-ramp){ /*v = -c*current_r² + d */
             delay = dr/( -c*current_r*current_r + d);
-            nsleep(delay);
+            msleep(delay);
         }
     }
     /* end of mess */
     }
 }
 
-
 int main()
 {
     int i1;
     double j;
     double dummy = 70;
-
-
+    /* different positions to play with */
     struct Pos start;
     struct Pos stop;
     struct Pos bottom1;
@@ -135,12 +134,12 @@ int main()
     midhigh.x=0; midhigh.y=20; midhigh.z=30;
     stopmid.x=10; stopmid.y=30; stopmid.z=15;
 
-
-    fd = serialport_init("/dev/ttyUSB0", 115200);
+    fd = serialport_init("/dev/ttyUSB0", 115200); /* open the serial port */
     sleep(2);
+
     inverseKinematics(0,20,10,t,angles);
     commandArduino(fd,angles);
-    sleep(15);
+    sleep(10);
 
     line(midlow,mid);
     line(mid,bottom1);
@@ -151,71 +150,68 @@ int main()
     line(stop,bottom2);
     line(bottom2,mid);
 
+    /* no function yet to go from 1 orientation to another */
     for (j=0; j<=dummy; j++){
         eulerMatrix(0,0,(j/dummy)*pi/4,t);
         inverseKinematics(0,25,20,t,angles);
         commandArduino(fd,angles);
-        nsleep(20);
+        msleep(20);
     }
 
     for (j=-dummy; j<=dummy; j++){
         eulerMatrix(0,0,-(j/dummy)*pi/4,t);
         inverseKinematics(0,25,20,t,angles);
         commandArduino(fd,angles);
-        nsleep(20);
+        msleep(20);
     }
 
     for (j=dummy; j>=0; j--){
         eulerMatrix(0,0,-(j/dummy)*pi/4,t);
         inverseKinematics(0,25,20,t,angles);
         commandArduino(fd,angles);
-        nsleep(20);
+        msleep(20);
     }
 
     for (j=0; j>=-dummy; j--){
         eulerMatrix((j/dummy)*pi/4,0,0,t);
         inverseKinematics(0,25,20,t,angles);
         commandArduino(fd,angles);
-        nsleep(20);
+        msleep(20);
     }
 
     for (j=-dummy; j<=dummy; j++){
         eulerMatrix((j/dummy)*pi/4,0,0,t);
         inverseKinematics(0,25,20,t,angles);
         commandArduino(fd,angles);
-        nsleep(20);
+        msleep(20);
     }
-
-
 
     for (j=0; j<=dummy; j++){
         eulerMatrix(cos((j/dummy)*pi)*pi/4,0,sin((j/dummy)*pi)*pi/4,   t);
         inverseKinematics(0,25,20,t,angles);
         commandArduino(fd,angles);
-        nsleep(50);
+        msleep(50);
     }
+
     for (j=0; j<=dummy; j++){
         eulerMatrix(-cos((j/dummy)*pi)*pi/4,0,-sin((j/dummy)*pi)*pi/4,   t);
         inverseKinematics(0,25,20,t,angles);
         commandArduino(fd,angles);
-        nsleep(50);
+        msleep(50);
     }
-    nsleep(500);
+    msleep(500);
     for (j=dummy; j>=0; j--){
         eulerMatrix((j/dummy)*pi/4,0,0,t);
         inverseKinematics(0,25,20,t,angles);
         commandArduino(fd,angles);
-        nsleep(20);
+        msleep(20);
     }
-
-
 
     line(mid,midlow);
     line(midlow,midfront);
     line(midfront,midlow);
     line(midlow,midhigh);
     line(midhigh,midlow);
-
 
     sleep(2);
     serialport_close(fd);

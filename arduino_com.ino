dd71e2e659ae90ae1eea5f7bdc417fea2c49e4f2
Thomas <thomas-hiemstra@hotmail.com>
2016-11-20 00:00:48 +0100

    added arduino code

---
+++ arduino_com.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+int data[7];
+int i;
+int counter = 0;
+
+int readStuff(){
+  byte MSB = 0;  // to build  2 byte integer from serial in byte
+  byte LSB = 0;  // to build  2 byte integer from serial in byte
+  short   MSBLSB = 0;  //to build  2 byte integer from serial in byt
+  MSB = Serial.read();
+  LSB = Serial.read();
+  MSBLSB=word(MSB, LSB);  
+  return MSBLSB;
+}
+
+
+void setup(){
+  pwm.begin();
+  pwm.setPWMFreq(60);
+  Serial.begin(115200); 
+}
+
+void loop(){
+  if (Serial.available() >= 2){  // wait for 2 bytes.
+    data[counter]=readStuff(); 
+    counter++;
+  }
+  if(counter == 7){
+    counter = 0;
+    for(i=0; i<7; i++){
+      Serial.println(data[i]);
+      pwm.setPWM(i, 0, data[i]);
+    }
+  }
+}
+
+
